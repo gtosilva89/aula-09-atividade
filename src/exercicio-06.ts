a4abb9223f27683c39b38f29fd258a21fda24eeb
Gustavo Silveira Silva <silva.gustavo@hotmail.com.br>
2024-11-28 21:53:30 -0300

    exercicio 6

--- src/exercicio-06.ts
+++ src/exercicio-06.ts
@@ -2,3 +2,14 @@
 // iniciando a contagem em dez graus Celsius e finalizando em cem graus Celsius.
 // O programa deve apresentar os valores das duas temperaturas.
 // Para converter grau Celsius para Fahrenheit, basta multiplicar a temperatura em graus Celsius por 1,8 e somar 32
+
+let celsius = 0;
+let fahrenheit =0;
+
+for(let i = 1; i<=100; i++){
+  if(!(i%10)){
+  celsius = i;
+  fahrenheit = (celsius*1.8) +32;
+  console.log("Graus Célcius =" + celsius + "Graus Fahrenheit= "+ fahrenheit);
+}
+}
