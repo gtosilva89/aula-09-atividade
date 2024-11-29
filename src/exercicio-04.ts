6256836a9823825ae4121bf34d404b3fe853f9a2
Gustavo Silveira Silva <silva.gustavo@hotmail.com.br>
2024-11-28 21:28:53 -0300

    exercicio 4

--- src/exercicio-04.ts
+++ src/exercicio-04.ts
@@ -3,3 +3,11 @@
 // Sugestão: leve em consideração as definições matemáticas do cálculo de potência,
 // em que qualquer valor numérico diferente de zero elevado a zero é 1, e todo valor numérico elevado a 1 é ele próprio.
 // Não use o operador aritmético de exponenciação “Math.pow” e resolva o problema com a técnica de laço a sua escolha.
+
+
+let base = 3;
+let resultado = 1;
+for (let i = 0; i <= 15; i++) {
+      console.log("O número " + base + " elevado à base " + i + " é = " + resultado) ;
+      resultado *= base;
+}
